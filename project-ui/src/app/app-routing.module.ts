14c976d11f0bfb2be7e7eb6cc0e587123ea8c5d9
pakman6392 <pakman6392@gmail.com>
2022-04-13 20:49:35 -0500

    Adding Book Catalog
    Adding UI component for the book catalog

--- project-ui/src/app/app-routing.module.ts
+++ project-ui/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LocationComponent } from './location/location.component';
+import { BooklistComponent } from './booklist/booklist.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
   { path: 'location', component: LocationComponent },
+  { path: 'booklist', component: BooklistComponent },
 ];
 
 @NgModule({
--- project-ui/src/app/app.module.ts
+++ project-ui/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LocationComponent } from './location/location.component';
+import { BooklistComponent } from './booklist/booklist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LocationComponent
+    LocationComponent,
+    BooklistComponent
   ],
   imports: [
     BrowserModule,
