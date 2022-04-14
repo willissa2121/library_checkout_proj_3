fbd93c257cc4f166606ca3bbf0100a31cd18c561
pakman6392 <pakman6392@gmail.com>
2022-04-13 20:45:26 -0500

    UI Update
    Updated to have location section

--- project-ui/src/app/app-routing.module.ts
+++ project-ui/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LocationComponent } from './location/location.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
+  { path: 'location', component: LocationComponent },
 ];
 
 @NgModule({
