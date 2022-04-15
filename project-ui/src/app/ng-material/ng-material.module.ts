d8a10ff54dac885360adbc529bf37cc65172aff3
subash.kharel.123@outlook.com <librarycheckoutproj@gmail.com>
2022-04-15 01:07:55 -0500

    added angular material modules

---
+++ project-ui/src/app/ng-material/ng-material.module.ts
@@ -0,0 +1,57 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatListModule} from '@angular/material/list';
+
+
+
+
+
+@NgModule({
+    declarations: [],
+    imports: [
+        CommonModule,
+        MatTableModule,
+        MatSortModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatIconModule,
+        MatSelectModule,
+        MatRadioModule,
+        MatButtonModule,
+        MatCardModule,
+        MatListModule
+    ],
+    schemas: [
+      CUSTOM_ELEMENTS_SCHEMA
+    ],
+    exports: [
+        CommonModule,
+        MatTableModule,
+        MatSortModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatIconModule,
+        MatSelectModule,
+        MatRadioModule,
+        MatButtonModule,
+        FormsModule,
+    ]
+})
+export class NgMaterialModule { }
