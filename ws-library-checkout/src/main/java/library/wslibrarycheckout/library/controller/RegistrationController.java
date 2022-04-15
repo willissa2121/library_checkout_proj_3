120bca2986bbc8cb3d4f9638ac402fe787d0e8c0
subash.kharel.123@outlook.com <librarycheckoutproj@gmail.com>
2022-04-15 01:08:55 -0500

    added guard for CORS and added api for registration

--- ws-library-checkout/src/main/java/library/wslibrarycheckout/library/controller/RegistrationController.java
+++ ws-library-checkout/src/main/java/library/wslibrarycheckout/library/controller/RegistrationController.java
@@ -8,15 +8,13 @@ import library.wslibrarycheckout.library.model.LogInDTO;
 import library.wslibrarycheckout.library.service.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping
+@CrossOrigin(origins = "http://localhost:4200")
 public class RegistrationController {
 
 
