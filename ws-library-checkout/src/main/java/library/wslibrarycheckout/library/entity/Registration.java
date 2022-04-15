1de913aeead6f2112f570a81eff037ceec2fbe61
subash.kharel.123@outlook.com <librarycheckoutproj@gmail.com>
2022-04-14 21:26:59 -0500

    added registration model

--- ws-library-checkout/src/main/java/library/wslibrarycheckout/library/entity/Registration.java
+++ ws-library-checkout/src/main/java/library/wslibrarycheckout/library/entity/Registration.java
@@ -1,8 +1,15 @@
 package library.wslibrarycheckout.library.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Registration {
 
     @Id
