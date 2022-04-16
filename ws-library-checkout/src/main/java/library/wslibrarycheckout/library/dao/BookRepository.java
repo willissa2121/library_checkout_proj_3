04c0052575eb86324eb2f0373752fd0509cb345a
subash.kharel.123@outlook.com <librarycheckoutproj@gmail.com>
2022-04-16 08:39:53 -0500

    added delete functionality in ui and also create delete api by isnb and configured to autorefresh the ui on delete

--- ws-library-checkout/src/main/java/library/wslibrarycheckout/library/dao/BookRepository.java
+++ ws-library-checkout/src/main/java/library/wslibrarycheckout/library/dao/BookRepository.java
@@ -4,6 +4,9 @@ import library.wslibrarycheckout.library.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book,Integer> {
+   Optional<Book> findByIsbn(String isbn);
 }
