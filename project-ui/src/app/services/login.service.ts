0d0b7b352679c13ce32945b71e6904d87fbab7a9
subash.kharel.123@outlook.com <librarycheckoutproj@gmail.com>
2022-04-16 08:03:57 -0500

    added user session, routing to catalogue page...

--- project-ui/src/app/services/login.service.ts
+++ project-ui/src/app/services/login.service.ts
@@ -1,13 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
+  public isUserLoggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  
+
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private router: Router
   ) { }
 
  async registerUser(value: any){
@@ -30,6 +36,11 @@ export class LoginService {
       (response) =>{
         let respJson = JSON.stringify(response);
         let respParseJson = JSON.parse(respJson);
+        const session = localStorage.setItem("userName", value.userName);
+        this.isUserLoggedIn.next(true);
+        this.router.navigate(['/catalogue']);
+
+
         alert(respParseJson.message[0])
       },
       (error) => {
