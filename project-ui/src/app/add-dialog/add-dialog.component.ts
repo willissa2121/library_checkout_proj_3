bfeb879f80aa0214cef47ec1103287e05f293d18
subash.kharel.123@outlook.com <librarycheckoutproj@gmail.com>
2022-04-16 22:11:08 -0500

    added the add modal for book

--- project-ui/src/app/add-dialog/add-dialog.component.ts
+++ project-ui/src/app/add-dialog/add-dialog.component.ts
@@ -1,8 +1,26 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { BookService } from '../services/book.service';
 import { DialogData } from '../update-dialog/update-dialog.component';
+
+// export class Book {
+//   name: string;
+//   authorEmail: string;
+//   edition: string;
+//   availability: string;
+//   expectedReturnDate: Date;
+//   isbn: string;
+//   author: Author;
+
+
+// }
+
+// export class Author {
+//   fullName: String;
+//   email: String;
+// }
 
 @Component({
   selector: 'app-add-dialog',
@@ -12,17 +30,36 @@ import { DialogData } from '../update-dialog/update-dialog.component';
 export class AddDialogComponent implements OnInit {
 
   constructor(private fb: FormBuilder, public dialogRef: MatDialogRef<AddDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData, private bookService: BookService) { }
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+     private bookService: BookService, private router: Router,
+     private changeDetector: ChangeDetectorRef) { }
 
   ngOnInit(): void {
+    this.formdata
   }
 
   formdata = new FormGroup({
-    name: new FormControl(this.data.book.name),
-    isbn: new FormControl(this.data.book.isbn),
-    edition: new FormControl(this.data.book.edition),
-    expectedReturnDate: new FormControl(this.data.book.expectedReturnDate),
-    author: new FormControl(this.data.book.author.fullName),
-    availability: new FormControl(this.data.book.availability),
+    name: new FormControl(''),
+    isbn: new FormControl(''),
+    edition: new FormControl(''),
+    expectedReturnDate: new FormControl(''),
+    authorEmail: new FormControl(''),
+    availability: new FormControl(''),
+    fullName: new FormControl(''),
+    email: new FormControl(''),
 });
+
+addBook(data: any){
+  
+  console.log("add dialog---->",data)
+  this.bookService.addBook(data).subscribe(
+    resp => {
+      console.log("book added")
+      this.dialogRef.close
+      this.changeDetector.detectChanges
+      this.router.navigate(['/catalogue'])
+
+    }
+  );
+}
 }
